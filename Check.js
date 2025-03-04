00cfa01475cac90b4d0f0a67b31729f5a0ec2d40
amanchourasiya13 <amanchourasiya643@gmail.com>
2025-03-04 12:15:38 +0530

    [Aman] Calculating wages for a Month.

--- Check.js
+++ Check.js
@@ -1,23 +1,28 @@
-//refractor the code to write a function to get work hours.
+//Calculating Wages for a Month assuming 20 Working Days in a Month
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
+// Function to get working hours based on employee type
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case IS_PART_TIME:
             return PART_TIME_HOURS;
         case IS_FULL_TIME:
-            return FULL_TIME_HOURS;   
-    
+            return FULL_TIME_HOURS;
         default:
             return 0;
     }
 }
+
+const NUM_OF_WORKING_DAYS=20;
 let empHrs=0;
-let empCheck=Math.floor(Math.random()*10)%3;
-empHrs=getWorkingHours(empCheck);
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck=Math.floor(Math.random()*10)%3;
+     empHrs+=getWorkingHours(empCheck);
+}
 let empWage=empHrs*WAGE_PER_HOUR;
-console.log("Emp Wage: "+empWage);
+console.log("Total Hrs: "+empHrs+" Emp Wage: "+empWage);
